c761698abfdc2da70f1322a9c21f6ded22cd710c
lyyang <710340968@qq.com>
2022-03-04 21:29:56 +0800

    Add files via upload

--- run_eval.sh
+++ run_eval.sh
@@ -10,7 +10,7 @@ python train_demo.py \
     --trainN $N --N $N --K $K --Q 1 --dot \
     --model proto --encoder bert --hidden_size 768 --val_step 1000 --test $N-$K-test-relid \
     --batch_size 4 --only_test \
-    --load_ckpt ./checkpoint/$ModelType/camery-ready-CP-$N-$K.pth.tar \
+    --load_ckpt ./checkpoint/$ModelType/camery-ready-$N-$K.pth.tar \
     --pretrain_ckpt /data/liuyang/97beifen/liuyang/bert-base-uncased \
     --cat_entity_rep \
     --test_iter 1000 \
