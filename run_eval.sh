f6d9893065f23b901589008d4d17848adde63467
lyyang <710340968@qq.com>
2022-03-04 20:00:28 +0800

    Add files via upload

---
+++ run_eval.sh
@@ -0,0 +1,18 @@
+ModelType=nodropPrototype-nodropRelation-lr-5e-6
+#nodropPrototype-dropRelation-lr-1e-5
+#dropPrototype-nodropRelation-lr-2e-5
+#nodropPrototype-nodropRelation-lr-1e-5
+#acl-camera-ready-$N-$K.pth.tar
+N=5
+K=1
+
+python train_demo.py \
+    --trainN $N --N $N --K $K --Q 1 --dot \
+    --model proto --encoder bert --hidden_size 768 --val_step 1000 --test $N-$K-test-relid \
+    --batch_size 4 --only_test \
+    --load_ckpt ./checkpoint/$ModelType/camery-ready-CP-$N-$K.pth.tar \
+    --pretrain_ckpt /data/liuyang/97beifen/liuyang/bert-base-uncased \
+    --test_output ./submit/$ModelType/pred-$N-$K.json \
+    --cat_entity_rep \
+    --test_iter 1000 \
+    --backend_model bert
---
+++ run_train.sh
@@ -0,0 +1,16 @@
+LR=1e-5
+ModelType=nodropPrototype-nodropRelation-lr-$LR
+#nodropPrototype-dropRelation-lr-1e-5
+#dropPrototype-nodropRelation-lr-2e-5
+#nodropPrototype-nodropRelation-lr-1e-5
+#acl-camera-ready-$N-$K.pth.tar
+N=5
+K=1
+
+python train_demo.py \
+    --trainN $N --N $N --K $K --Q 1 --dot \
+    --model proto --encoder bert --hidden_size 768 --val_step 1000 --lr $LR \
+    --pretrain_ckpt /data/liuyang/97beifen/liuyang/bert-base-uncased \
+    --batch_size 4 --save_ckpt ./checkpoint/$ModelType/camery-ready-CP-$N-$K.pth.tar \
+    --cat_entity_rep \
+    --backend_model bert
